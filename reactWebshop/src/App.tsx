37307a15981795a08e85a695a753915b8e1625a2
Kristian Mortensen <kristianbmortensen@gmail.com>
2024-12-10 02:28:46 +0100

    push 1
    sdf

---
+++ reactWebshop/src/App.tsx
@@ -0,0 +1,13 @@
+import Item from './components/item.tsx';
+
+function App() {
+
+  return(
+  <>
+    <Item></Item>
+    <Item></Item>
+    </>
+  );
+
+}
+export default App
---
+++ reactWebshop/src/components/Item.tsx
@@ -0,0 +1,17 @@
+
+function Item(){
+    return(
+        <>
+        <h1 id = "itemn-name">Item name</h1>
+        <p id = "item-description">Item description</p>
+        <img id = "item-image"></img>
+        <p id = "item-price">Item price</p>
+        <p id = "item-reviewscore">Item review score</p>
+        <p id = "item-reviewcount">Item review count</p>
+        <p id = "item-amountinstock">Item amount in stock</p>
+        </>
+        
+    );
+}
+
+export default Item;

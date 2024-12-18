3128e5e3e7f9cbb380955fec519c12812a9bd192
Kristian Mortensen <kristianbmortensen@gmail.com>
2024-12-19 00:28:10 +0100

    slight update
    very minor update

--- reactWebshop/src/components/Item.tsx
+++ reactWebshop/src/components/Item.tsx
@@ -9,7 +9,7 @@ function Item({ data }: { data: any }){
         <h1>{data.title}</h1>
         <p>{data.description}</p>
         <img src = {data.imgLink} height="200px" alt = "item image missing :("></img>
-        <p >price: {data.price ? data.price : "log in to see prices! :D"},-</p>
+        <p >price: {data.price ? data.price + ",-" : "log in to see prices! :D"}</p>
         <p>review score: {data.rating}</p>
         <p>reviews: {data.amountOfRating}</p>
         <p>quantity: {data.quantity}</p>
